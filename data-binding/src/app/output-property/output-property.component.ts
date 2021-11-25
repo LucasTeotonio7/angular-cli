e1e16e28962a9208169a340013d82a614ace8ddf
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-25 07:28:19 -0300

    A16-DOM and template access with viewChild

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'counter',
@@ -11,13 +11,15 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() changeValue = new EventEmitter();
 
+  @ViewChild('fieldInput') fieldValueInput!: ElementRef;
+
   increment(){
-    this.value++;
+    this.fieldValueInput.nativeElement.value++;
     this.changeValue.emit({newValue: this.value})
   }
 
   decrement(){
-    this.value--;
+    this.fieldValueInput.nativeElement.value--;
     this.changeValue.emit({newValue: this.value})
   }
 
