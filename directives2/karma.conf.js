6451a6151d575fc880034ba871a7fe31713d319e
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-11-29 21:38:34 -0300

    rename directives

--- directives2/karma.conf.js
+++ directives2/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/directives'),
+      dir: require('path').join(__dirname, './coverage/directives2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- directives2/src/app/app.component.spec.ts
+++ directives2/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'directives'`, () => {
+  it(`should have as title 'directives2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('directives');
+    expect(app.title).toEqual('directives2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('directives app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('directives2 app is running!');
   });
 });
