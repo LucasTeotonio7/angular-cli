8a0f99e83cd7b21e133a5a641dea6f3ddc968437
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-13 15:28:22 -0300

    A30-directive ngStyle

--- directives2/src/app/directive-ngstyle/directive-ngstyle.component.ts
+++ directives2/src/app/directive-ngstyle/directive-ngstyle.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectiveNgstyleComponent implements OnInit {
 
+  active: boolean = false;
+  fontSize: number = 10;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeActive(){
+    this.active = !this.active;
   }
 
 }
