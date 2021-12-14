3d53195dc001a8bdf72240f4a5405122d8fc9ab0
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-14 20:04:42 -0300

    A33-created yellow background directive

---
+++ directives2/src/app/shared/yellow-background.directive.spec.ts
@@ -0,0 +1,8 @@
+import { YellowBackgroundDirective } from './yellow-background.directive';
+
+describe('YellowBackgroundDirective', () => {
+  it('should create an instance', () => {
+    const directive = new YellowBackgroundDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ directives2/src/app/shared/yellow-background.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[YellowBackground]'
+})
+export class YellowBackgroundDirective {
+
+  constructor() { }
+
+}
