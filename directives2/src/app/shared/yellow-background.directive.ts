fdc254822e9064c68b40add243a876108b080f10
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-14 20:28:15 -0300

    A33-created attribute directive

--- directives2/src/app/shared/yellow-background.directive.ts
+++ directives2/src/app/shared/yellow-background.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[YellowBackground]'
+  selector: 'p[yellowBackground]'
 })
 export class YellowBackgroundDirective {
 
-  constructor() { }
+  constructor(private _elementRef: ElementRef,
+              private _renderer: Renderer2) {
+    // console.log(this._elementRef);
+    // this._elementRef.nativeElement.style.backgroundColor = 'Yellow';
+    this._renderer.setStyle(
+      this._elementRef.nativeElement,
+      'background-color', 'yellow'
+    )
+   }
 
 }
