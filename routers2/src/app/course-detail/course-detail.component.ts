886e1b423bb47be619b324f4ee15325d8727ce14
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2021-12-28 22:46:41 -0300

    A54-Listening to changes in routing parameters

--- routers2/src/app/course-detail/course-detail.component.ts
+++ routers2/src/app/course-detail/course-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-course-detail',
@@ -9,12 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 export class CourseDetailComponent implements OnInit {
 
   id: any = '';
+  subscription!: Subscription;
 
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.paramMap.get('id');
+    // this.id = this.route.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(
+      (params: any) =>{
+        this.id = params['id']
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
