d6d72600b7d80bed10c4c2f56249bf619cbee977
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-01-16 21:44:18 -0300

    A73-forms: created initial project

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+
+const routes: Routes = [
+  {path:'templateForm', component: TemplateFormComponent},
+  {path:'dataForm', component: DataFormComponent},
+  {path:'', pathMatch: 'full', redirectTo: 'templateForm'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
