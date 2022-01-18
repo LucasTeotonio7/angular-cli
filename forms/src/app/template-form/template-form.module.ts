a8db05049ba140edfd02b3a943c4b743c1a5f1ba
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-01-17 21:46:34 -0300

    A81-forms: (tip) checking Form data in template with JSON

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 
 
 @NgModule({
   declarations: [
     TemplateFormComponent,
+    FormDebugComponent,
   ],
   imports: [
     CommonModule,
