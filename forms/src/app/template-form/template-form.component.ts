8619e6aa5ef8d14227532b39fa52e1860e18f107
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-01-18 23:25:06 -0300

    A85-forms: address requests

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-template-form',
@@ -18,7 +20,7 @@ export class TemplateFormComponent implements OnInit {
     console.log(this.user)
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -29,6 +31,28 @@ export class TemplateFormComponent implements OnInit {
 
   ApplyErrorClass(field: any){
     return { 'is-invalid': this.checkTouched(field) };
+  }
+
+  searchCep(cep: any){
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      if(validacep.test(cep)) {
+
+        this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe(
+          data => console.log(data)
+        );
+
+      }
+
+    }
+
   }
 
 }
--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -14,6 +15,7 @@ import { FormDebugComponent } from '../form-debug/form-debug.component';
   imports: [
     CommonModule,
     FormsModule,
+    HttpClientModule,
   ]
 })
 export class TemplateFormModule { }
