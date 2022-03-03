3301b056df15307c3f6ac33fb5f4a220c2a48f0c
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-03-02 22:19:46 -0300

    A136-http: file upload: backend with Node.js

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParse = require('body-parser');
+const multipart = require('connect-multiparty');
+
+const app = express();
+
+// TODO: BODY-PARSE
+app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({ extended: true }))
+
+// TODO: CORS
+const corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200,
+
+};
+
+app.use(cors(corsOptions));
+
+// TODO: MULTIPART
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
+app.post('/upload', multipartMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({ message: files });
+});
+
+app.use((err, res, req, next) => res.json({ error: err.message }))
+
+app.listen(8000, () => {
+  console.log('Servidor porta 8000');
+});
