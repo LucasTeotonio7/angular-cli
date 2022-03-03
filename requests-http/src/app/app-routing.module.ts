2e2de79ea50abdf33861ab09933ccfc205cb3e48
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-03-02 21:47:06 -0300

    A135-file upload: form upload with Bootstrap

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'courses' },
-  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)},
+  { path: '', pathMatch: 'full', redirectTo: 'upload' },
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./courses/courses.module').then((m) => m.CoursesModule),
+  },
+  {
+    path: 'upload',
+    loadChildren: () =>
+      import('./upload-file/upload-file.module').then((m) => m.UploadFileModule),
+  },
   {
     path: 'rxjs-poc',
-    loadChildren: () => import('././unsubscribe-rxjs/unsubscribe-rxjs.module').then(m => m.UnsubscribeRxjsModule)
-  }
+    loadChildren: () =>
+      import('././unsubscribe-rxjs/unsubscribe-rxjs.module').then(
+        (m) => m.UnsubscribeRxjsModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
